8b2e5f1d88b41704206d59ba80d1947c5db1edf9
Rodrigo Dias <diasbass@hotmail.com>
2020-10-21 22:21:35 -0300

    fix main pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React, { useEffect } from 'react';
-import axios from 'axios';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import './main.scss';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
-}
+import Header from './components/Header';
+import Main from './pages/main';
+import Product from './pages/product';
+import Footer from './components/Footer';
+
+
+const App = () => (
+  <div className="App">
+    <Header title="Lista de vinhos" />
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/">
+              <Main />
+            </Route>
+            <Route exact path="/detail/:id">
+              <Product />
+            </Route>
+          </Switch>
+        </BrowserRouter>
+        <Footer text="Footer" />
+  </div>
+);
 
 export default App;
